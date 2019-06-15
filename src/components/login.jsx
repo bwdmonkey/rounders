6d50362bdf64d48e77cb72de3475babc1e15a257
Tom Lee <win981026@gmail.com>
2019-06-14 19:12:52 -0700

    feat: Added basic login form

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class Login extends Component {
     render() {
         return (
             <div className="login">
-                <h1>
-                    Login
-                </h1>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
 
-                <p>
-                    This is the login page!
-                </p>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Login
+                    </Button>
+                </Form>
             </div>
         );
     }
