e624a88ca3ba20c099ecaac988ec0800071986b0
Tom Lee <win981026@gmail.com>
2019-06-14 19:08:39 -0700

    feat: Added basic register form

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
-
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 class Register extends Component {
     render() {
         return (
             <div className="register">
-                <h1>
-                    Register
-                </h1>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
 
-                <p>
-                    This is the register page!
-                </p>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicBankAccount">
+                        <Form.Label>Bank Account Information</Form.Label>
+                        <Form.Control className="m-b-5" type="text" placeholder="Institute Name - eg. CIBC" />
+                        <Form.Control className="m-b-5" type="text" placeholder="Institute Number - eg. 010" />
+                        <Form.Control className="m-b-5" type="text" placeholder="Transit Number - eg. 05800" />
+                        <Form.Control className="m-b-5" type="text" placeholder="Account Number - eg. 1234567" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicChecbox">
+                        <Form.Check type="checkbox" className="text-muted" label="By checking this box, you agree to grade this project with generousity." />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
             </div>
         );
     }
