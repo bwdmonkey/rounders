2f7e90de14a40668efa3bbb6ee37d9d0e12ee4d8
sihan <h6o1b@ugrad.cs.ubc.ca>
2019-06-19 02:36:49 -0700

    articles.jsx

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -4,22 +4,31 @@ import Button from 'react-bootstrap/Button';
 
 function getJson() {
     // JSON PARSER
-    return [{ "ID": "1", "Username": "SmoothieX", "Written_At": "2999-01-08 04:05:06", "Title": "Dinner", "Content": "Today I ate dinner at McDonalds"}, 
-            { "ID": "2", "Username": "Smoothief", "Written_At": "2999-01-08 04:05:06", "Title": "Dinner Again", "Content": "Second dinner at McDonalds"}];
+    return [{ "ID": "1", "username": "SmoothieX", "created_at": "2999-01-08 04:05:06", "title": "Dinner", "Content": "Today I ate dinner at McDonalds"}, 
+            { "ID": "2", "username": "Smoothief", "created_at": "2999-01-08 04:05:06", "title": "Dinner Again", "Content": "Second dinner at McDonalds"}];
   }
 
 class Articles extends Component {
     constructor(props) {
         super(props)
-        this.state = { json: [] }
+        this.state = { json: [], }
     }
 
-    componentDidMount() {
-        this.setState((prevState) => {
-            return {
-                json: getJson()
-            }
-        })
+    searchTitle = (event) => {
+        const form = event.currentTarget;
+        event.preventDefault();
+
+        fetch('/articles?title=' + encodeURIComponent(form.elements.formArticleFilter.value))
+            .then(res => res.json())
+            .then(json => {this.setState({ json: json.result })})
+            .catch(console.log);
+    }
+
+    componentDidMount = () => {
+        fetch('/articles?title=')
+            .then(res => res.json())
+            .then(json => {this.setState({ json: json.result })})
+            .catch(console.log);
     }
 
     render() {
@@ -28,24 +37,25 @@ class Articles extends Component {
                 <h1>Articles</h1>
                 
                 <div id="articleFilter">
-                        <Form.Group controlId="formarticleFilter">
+                    <Form onSubmit={this.searchTitle}>
+                        <Form.Group controlId="formArticleFilter">
                             <Form.Label>Search Title Keywords</Form.Label>
                             <Form.Control className="m-b-5" type="text" placeholder="Keywords" />
                         </Form.Group>
                         <Button variant="primary" type="submit">
                             Filter
                         </Button>
+                    </Form>
                 </div>
 
                 {this.state.json.map((data, i) => {
                     return (
-                    <p key={i}>
-                        <hr></hr>
+                    <div key={i}>
                         {/* <td>{data.ID}</td> */}
-                        <h3>{data.Title}</h3>
-                        <h4>By {data.Username} -- {data.Written_At}</h4>
-                        <h5>{data.Content}</h5>
-                    </p>
+                        <h3>{data.title}</h3>
+                        <h4>By {data.username} -- {data.created_at}</h4>
+                        <h5>{data.content}</h5>
+                    </div>
                     )
                 })}
             </div>
