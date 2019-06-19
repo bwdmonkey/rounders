16c3eae889411e972bbf24b172f6c0b5b136a181
Tom Lee <win981026@gmail.com>
2019-06-18 18:52:40 -0700

    feat: Added admin page backend conn

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -5,70 +5,35 @@ class Admin extends Component {
     super(props);
 
     this.state = {
-      response: {
-        // [...]
-        body: {
-          newUsersLastWeek: 1,
-          newArticlesLastWeek: 1,
-          newClapsLastWeek: 666,
-          avgClapsPerUser: 34,
-          avgClapsPerArticle: 4.2,
-          avgArticlesPerUser: 0.2,
-          noArticles: {
-            all: 16.5,
-            finance: 1.1,
-            economics: 2.2,
-            life: 3.3,
-            tech: 4.4,
-            funding: 5.5,
-          },
-        },
+      data: {
+        newUsersLastWeek: 0,
+        newArticlesLastWeek: 0,
+        newClapsLastWeek: 0,
+        avgClapsPerUser: 0,
+        avgClapsPerArticle: 0,
+        avgArticlesPerUser: 0,
       },
     };
   }
 
-  toCapitalCase = str => str.charAt(0).toUpperCase() + str.slice(1)
-
-  mapTagToIconClass = tag => ({
-    all: 'fa fa-cubes thin-icon',
-    finance: 'fa fa-credit-card thin-icon',
-    economics: 'fa fa-handshake-o thin-icon',
-    life: 'fa fa-heart-o',
-    tech: 'fa fa-laptop',
-    funding: 'icon-line-chart',
-  }[tag])
-
-  generateCards = (noArticlesObj) => {
-    const components = [];
-    Object.keys(noArticlesObj).forEach((key) => {
-      components.push(this.generateCard(key, noArticlesObj[key]));
-    });
-    const cards = [];
-    for (let i = 0; i < components.length; i += 3) {
-      const row = [];
-      for (let j = 0; j < 3; j += 1) {
-        if (components[i + j]) row.push(components[i + j]);
-      }
-      cards.push(<div className="row d-flex">{row}</div>);
-      cards.push(<br />);
-    }
-    return cards;
+  componentDidMount() {
+    // GET /analytics response format
+    // {
+    //   newUsersLastWeek: 1,
+    //   newArticlesLastWeek: 1,
+    //   newClapsLastWeek: 1,
+    //   avgClapsPerUser: 1,
+    //   avgClapsPerArticle: 1,
+    //   avgArticlesPerUser: 1,
+    // }
+    fetch('/analytics')
+      .then(res => res.json())
+      .then(data => this.setState({ data: data }))
+      .catch(_ => {});
   }
 
-  generateCard = (tag, num) => (
-    <div className="col-md-4" key="tag">
-      <div className="card income text-center">
-        <div className="icon"><i className={this.mapTagToIconClass(tag)} /></div>
-        <div className="number">{num}</div>
-        <strong className="text-primary">{this.toCapitalCase(tag)}</strong>
-      </div>
-    </div>
-  )
-
-
   render() {
-    const { response } = this.state;
-    const { body } = response;
+    const { data } = this.state;
 
     return (
       <div className="admin">
@@ -86,7 +51,7 @@ class Admin extends Component {
                   <div className="name">
                     <strong className="text-uppercase">New User</strong>
                     <span>Last 7 days</span>
-                    <div className="count-number">{body.newUsersLastWeek}</div>
+                    <div className="count-number">{data.newUsersLastWeek}</div>
                   </div>
                 </div>
               </div>
@@ -96,7 +61,7 @@ class Admin extends Component {
                   <div className="name">
                     <strong className="text-uppercase">New Articles</strong>
                     <span>Last 7 days</span>
-                    <div className="count-number">{body.newArticlesLastWeek}</div>
+                    <div className="count-number">{data.newArticlesLastWeek}</div>
                   </div>
                 </div>
               </div>
@@ -106,7 +71,7 @@ class Admin extends Component {
                   <div className="name">
                     <strong className="text-uppercase">New Claps</strong>
                     <span>Last 7 days</span>
-                    <div className="count-number">{body.newClapsLastWeek}</div>
+                    <div className="count-number">{data.newClapsLastWeek}</div>
                   </div>
                 </div>
               </div>
@@ -118,7 +83,7 @@ class Admin extends Component {
                   <div className="name">
                     <strong className="text-uppercase">Avg Claps / User</strong>
                     <span>Last 1 Month</span>
-                    <div className="count-number">{body.avgClapsPerUser}</div>
+                    <div className="count-number">{data.avgClapsPerUser}</div>
                   </div>
                 </div>
               </div>
@@ -128,7 +93,7 @@ class Admin extends Component {
                   <div className="name">
                     <strong className="text-uppercase">Avg Claps / Article</strong>
                     <span>Last 1 Month</span>
-                    <div className="count-number">{body.avgClapsPerArticle}</div>
+                    <div className="count-number">{data.avgClapsPerArticle}</div>
                   </div>
                 </div>
               </div>
@@ -138,18 +103,11 @@ class Admin extends Component {
                   <div className="name">
                     <strong className="text-uppercase">Avg Articles / User</strong>
                     <span>Last 1 Month</span>
-                    <div className="count-number">{body.avgArticlesPerUser}</div>
+                    <div className="count-number">{data.avgArticlesPerUser}</div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
-        {/* Statistics Section */}
-        <h1>Total Number of Articles</h1>
-        <section className="statistics">
-          <div className="container-fluid">
-            {this.generateCards(body.noArticles)}
           </div>
         </section>
       </div>
