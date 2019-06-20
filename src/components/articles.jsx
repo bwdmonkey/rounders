b43478bcbb80422d3c7c97ab156acd4058e15830
Tom Lee <win981026@gmail.com>
2019-06-19 23:13:07 -0700

    feat: Added clap reaction feature

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -6,33 +6,19 @@ class Articles extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            articles: [
-            {
-                article_id: 1,
-                username: "SmoothieX",
-                created_At: "2999-01-08 04:05:06",
-                title: "Dinner",
-                content: "Today I ate dinner at McDonalds",
-                clapNumber: 12
-            },
-            {
-                article_id: 2,
-                username:"Smoothief",
-                created_at:"2999-01-08 04:05:06",
-                title: "Dinner Again",
-                content: "Second dinner at McDonalds",
-                claps: 35
-            }],
-            claps: [
-            {
-                article_id: 1,
-                amount: 12
-            },
-            {
-                article_id: 2,
-                amount: 35
-            }], // mock data
+            user_id: localStorage.getItem('user_id'),
+            articleBeingClapped: null,
+            clapButtonIntervalId: null,
+            tmpClaps: 0,
+            articles: [],
         }
+    }
+
+    componentDidMount = () => {
+        fetch('/articles/deep')
+            .then(res => res.json())
+            .then(json => {this.setState({ articles: json.result })})
+            .catch(console.log);
     }
 
     searchTitle = (event) => {
@@ -45,52 +31,126 @@ class Articles extends Component {
             .catch(console.log);
     }
 
-    handleClaps = (event) => {
-        const form = event.currentTarget;
-        event.preventDefault();
-        // if (form.checkValidity() === false) {
-        //     event.stopPropagation();
-        // }
-        // this.setState({ validatedInfo: true });
-
-        const { article_id } = this.state;
-
-        let data = { amount: 14, } // amount + 1
-
-        fetch('/articles/' + article_id + '/reactions', {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data),
-        })
-        .catch(err => console.log("err", err));
+    getTotalClaps = (article) => {
+        let total = 0;
+        let reactions = article.reactions;
+        if (reactions.length > 0) {
+            for (let i = 0; i < reactions.length; i++) {
+                if (reactions[i].count !== undefined) {
+                    total += reactions[i].count
+                }
+            }
+        }
+        return total;
     }
 
-    componentDidMount = () => {
-        fetch('/articles')
-            .then(res => res.json())
-            .then(json => {this.setState({ articles: json.result })})
-            .catch(console.log);
+    getUserClaps = (article) => {
+        const { user_id } = this.state;
+        const { articleBeingClapped } = this.state;
+        const { tmpClaps } = this.state;
+        if (user_id === null) return 0;
+        if (article.id === articleBeingClapped) {
+            return tmpClaps;
+        }
+        let reactions = article.reactions;
+        if (reactions.length > 0) {
+            for (let i = 0; i < reactions.length; i++) {
+                if (reactions[i].user_id !== undefined && reactions[i].user_id == user_id) {
+                    return reactions[i].count
+                }
+            }
+        }
+        return 0;
+    }
 
-        let article_id = 1 // need to fill in
+    isContainedInReaction = (article) => {
+        const { user_id } = this.state;
+        if (user_id === null) return false;
+        let reactions = article.reactions;
+        if (reactions.length > 0) {
+            for (let i = 0; i < reactions.length; i++) {
+                if (reactions[i].user_id !== undefined && reactions[i].user_id == user_id) {
+                    return true
+                }
+            }
+        }
+        return false;
+    }
 
-        fetch('/articles/' + article_id + '/reactions')
-            .then(res => res.json())
-            .then(claps => {this.setState({ claps: claps.result })})
-            .catch(console.log);
 
-        // merge two sets of data
-        // let json = json;
-        // let claps = claps;
-        // let merged = [];
+    handleMouseDown = (article) => {
+        const { clapButtonIntervalId } = this.state;
 
-        // for(let i = 0; i < json.length; i++) {
-        //     merged.push({
-        //     ...json[i],
-        //     ...(claps.find((item) => item.article_id === json[i].id))});
-        // }
-        // console.log(merged);
+        if (clapButtonIntervalId === null) {
+            this.setState({
+                articleBeingClapped: article.id,
+                tmpClaps: this.getUserClaps(article),
+                clapButtonIntervalId: setInterval(this.handleMouseDownHelper, 200),
+            });
+        }
+    }
+
+    handleMouseDownHelper = () => {
+        this.setState(prev => ({
+            tmpClaps: Math.min(prev.tmpClaps + 1, 50)
+        }))
+    }
+
+    handleMouseEnd = (article) => {
+        const { user_id } = this.state;
+        const { clapButtonIntervalId, articleBeingClapped } = this.state;
+        if (clapButtonIntervalId !== null) {
+            clearInterval(clapButtonIntervalId)
+            if (this.isContainedInReaction(article)) {
+                let data = {
+                    count: this.getUserClaps(article)
+                }
+                fetch(`/reactions/${parseInt(user_id, 10)}/${articleBeingClapped}`, {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data),
+                }).then(_ => window.location.reload())
+                .catch(console.log);
+            } else {
+                let data = {
+                    user_id: parseInt(user_id, 10),
+                    article_id: articleBeingClapped,
+                    count: this.getUserClaps(article)
+                }
+                fetch('/reactions', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data),
+                }).then(_ => window.location.reload())
+                .catch(console.log);
+            }
+        }
+    }
+
+    generateClapButton = (article) => {
+        const { user_id } = this.state;
+
+        if (user_id === null) {
+            return (
+                <Button variant="primary" type="button" disabled>
+                    <i className="fa fa-hand-peace-o" aria-hidden="true"></i>
+                    &nbsp;
+                    0
+                </Button>
+            )
+        } else {
+            return (
+                <button type="button" className="btn btn-primary" onMouseDown={this.handleMouseDown.bind(this, article)} onMouseUp={this.handleMouseEnd.bind(this, article)} onMouseLeave={this.handleMouseEnd.bind(this, article)}>
+                    <i data-id={article.id.toString()} className="fa fa-hand-peace-o" aria-hidden="true"></i>
+                    &nbsp;
+                    {this.getUserClaps(article)}
+                </button>
+            )
+        }
     }
 
     render() {
@@ -115,16 +175,15 @@ class Articles extends Component {
             {/* TODO: if state.articles is empty, then display "sorry no articles found"*/}
                 {articles.map((data, i) => {
                     return (
-                    <div key={i} id="claps">
+                    <div key={'articleRow' + i} id="claps">
                         <hr></hr>
                         <h3>{data.title}</h3>
                         <h4>By {data.username} -- {data.created_at}</h4>
                         <h5>{data.content}</h5>
 
-                        <h4>Number of Claps For This Article: {data.clapNumber}</h4>
-                        <Button variant="primary" type="submit">
-                            <i class="fa fa-hand-peace-o" aria-hidden="true"></i>
-                        </Button>
+                        <h4>Number of Claps For This Article: {this.getTotalClaps(data)}</h4>
+
+                        {this.generateClapButton(data)}
                     </div>
                     )
                 })}
